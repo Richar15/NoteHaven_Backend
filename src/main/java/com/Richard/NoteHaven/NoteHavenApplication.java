ed73fbf7b9bd72ff256a9cd1592b05f5dda57ece
Richard15 <richardantonioassistrujillo@gmail.com>
2024-11-21 19:24:55 -0500

    first commit

---
+++ src/main/java/com/Richard/NoteHaven/NoteHavenApplication.java
@@ -0,0 +1,13 @@
+package com.Richard.NoteHaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteHavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Richard/NoteHaven/NoteHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Richard.NoteHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
